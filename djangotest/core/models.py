46c2ef4fcb335f9850aace7ba3c155fe76d869dc
MAkcanca <mustafa.the.science.guy@gmail.com>
2021-01-27 21:25:07 +0300

    Init

--- djangotest/core/models.py
+++ djangotest/core/models.py
@@ -1,3 +1,17 @@
+from django.utils.translation import ugettext_lazy as _
+
+
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+
+    title = models.CharField(_("Title"), max_length=50)
+    date = models.DateTimeField()
+    content = models.TextField()
+
+    class Meta:
+        verbose_name = _("blogpost")
+        verbose_name_plural = _("blogposts")
+
+    def __str__(self):
+        return f"{self.title} - {self.date}"  
--- djangotest/djangotest/settings.py
+++ djangotest/djangotest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dtqps_r=at_rsdb8r(ptqsmjvdt^%-t^6^7@5mt2_t%!7s+b7i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
